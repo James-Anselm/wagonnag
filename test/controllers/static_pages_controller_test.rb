99a54d0c6cb169110366e5bf96caef15dbce3f28
= <=>
2015-12-05 12:09:15 -0500

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Wagonnag - prevent the engine light"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Wagonnag - prevent the engine light"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Wagonnag - prevent the engine light"
+  end
 end
