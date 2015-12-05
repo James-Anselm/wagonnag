6023ccd55dbb95f26af1cd0c9172d53d5824cd46
= <=>
2015-12-05 12:17:03 -0500

    use setup method in test to store title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Wagonnag - prevent the engine light"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Wagonnag - prevent the engine light"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Wagonnag - prevent the engine light"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Wagonnag - prevent the engine light"
+    assert_select "title", "About | #{@base_title}"
   end
 end
