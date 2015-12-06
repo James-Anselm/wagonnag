537a5b28a26c3474e79df56309490cc8b157cc46
James Anselm <jamesanselm@gmail.com>
2015-12-06 00:13:03 -0500

    added full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Wagonnag - prevent the engine light"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
